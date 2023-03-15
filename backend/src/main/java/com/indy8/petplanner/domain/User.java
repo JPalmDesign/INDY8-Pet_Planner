2668a307b14020a3af66286d5dc19469c13519a0
Jessica Palmer <jwell115@students.kennesaw.edu>
2023-03-14 22:19:01 -0400

    Setting up mySQL database with spring boot and docker

--- backend/src/main/java/com/indy8/petplanner/domain/User.java
+++ backend/src/main/java/com/indy8/petplanner/domain/User.java
@@ -1,14 +1,67 @@
 package com.indy8.petplanner.domain;
 
-public class User {
-    String id;                  //Auto generate unique identifier
-    String firstName;
-    String lastName;
-    String email;
-    String username;
-    String password;        //Placeholder -- DO NOT STORE PLAIN TEXT PASSWORDS
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-    public User() {
-        id = "U";       // Auto generate a random 8 digit/alpha string with U at the front
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;                  //Auto generate unique identifier
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;        //Placeholder -- <masked> DO NOT STORE PLAIN TEXT PASSWORDS
+
+    public Integer getId() {
+        return id;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
