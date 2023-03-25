c8fa124e5759971fbc72e34efb2b9aa7ae1a5670
kkobus2 <kfelker2@gmail.com>
2023-03-25 16:31:30 -0400

    Basic authIO setup

--- frontend/pet_planner/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ frontend/pet_planner/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -15,5 +15,10 @@ import io.flutter.embedding.engine.FlutterEngine;
 public final class GeneratedPluginRegistrant {
   private static final String TAG = "GeneratedPluginRegistrant";
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    try {
+      flutterEngine.getPlugins().add(new com.auth0.auth0_flutter.Auth0FlutterPlugin());
+    } catch(Exception e) {
+      Log.e(TAG, "Error registering plugin auth0_flutter, com.auth0.auth0_flutter.Auth0FlutterPlugin", e);
+    }
   }
 }
