e5204e51e492e10330388dc7afb106c999c9c655
Sarah <92331459+sg00990@users.noreply.github.com>
2023-04-12 11:45:38 -0400

    Update exist_pet.dart

--- frontend/pet_planner/lib/pages/exist_pet.dart
+++ frontend/pet_planner/lib/pages/exist_pet.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pet_planner/pages/new_app.dart';
-import 'package:pet_planner/pages/new_pet.dart';
-import 'package:pet_planner/pages/schedule_page.dart';
-
-import 'forgot_password.dart';
-import 'login_page.dart';
+import 'package:pet_planner/pages/pet_view.dart';
 
 class OldPetPage extends StatelessWidget {
   const OldPetPage({super.key});
@@ -33,6 +28,55 @@ class OldPetPage extends StatelessWidget {
                 "Existing Pets",
                 style: TextStyle(fontFamily: 'robotoMedium', fontSize: 36),
               )),
+          const SizedBox(height: 50),
+          Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                    offset: const Offset(0, 3),
+                  )
+                ],
+              ),
+              child: SizedBox(
+                  width: 1035,
+                  height: 100,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: const Color(0xFFAEB2C5),
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            SizedBox(height: 80, width: 10),
+                            Icon(
+                              Icons.pets,
+                              color: Colors.black,
+                              size: 30,
+                            ),
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(10, 20, 20, 20),
+                                child: Text("Fluffy Smith", // pet.petName
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.black,
+                                        fontFamily: 'robotoMedium')))
+                          ],
+                        ),
+                      ],
+                    ),
+                    onPressed: () => Future(
+                      () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (_) => ViewPetPage()),
+                      ),
+                    ),
+                  )))
         ]));
   }
 }
