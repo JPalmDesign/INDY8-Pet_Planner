4b41c66c8ec90e22d77c5d1bb19bf3ec0fc02460
Jessica Palmer <jwell115@students.kennesaw.edu>
2023-04-13 17:56:58 -0400

    Add in swagger

--- backend/src/main/java/com/indy8/petplanner/PetPlannerApplication.java
+++ backend/src/main/java/com/indy8/petplanner/PetPlannerApplication.java
@@ -3,6 +3,7 @@ package com.indy8.petplanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PetPlannerApplication {
 
